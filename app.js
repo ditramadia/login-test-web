ef7295d489205f003289806fa64a1cdd76f968df
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-07-15 22:14:51 +0700

    server setup

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000.");
+})
